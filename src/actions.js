80a8e31798fb5f052c97f0ad64d06db191d6dabc
Daniel Friedman <dfriedman58@gmail.com>
2016-07-21 15:46:29 -0400

    Use redux, log in

---
+++ src/actions.js
@@ -0,0 +1,33 @@
+import solid from 'solid-client'
+
+export const LOG_IN_REQUEST = 'LOG_IN_REQUEST'
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS'
+export const LOG_IN_FAILURE = 'LOG_IN_FAILURE'
+
+export function logIn () {
+  return function (dispatch) {
+    dispatch(logInRequest())
+    return solid.login()
+      .then(webId => solid.getProfile(webId))
+      .then(profile => dispatch(logInSuccess(profile)))
+      .catch(error => dispatch(logInFailure(error)))
+  }
+}
+
+export function logInRequest () {
+  return { type: LOG_IN_REQUEST }
+}
+
+export function logInSuccess (profile) {
+  return {
+    type: LOG_IN_SUCCESS,
+    profile: profile
+  }
+}
+
+export function logInFailure (error) {
+  return {
+    type: LOG_IN_FAILURE,
+    profile: error
+  }
+}
--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
-import solid from 'solid-client'
+import { applyMiddleware, createStore } from 'redux'
+import thunkMiddleware from 'redux-thunk'
 
-console.log(solid)
+import { logIn } from './actions'
+import rootReducer from './reducers'
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
+
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
+store.dispatch(logIn())
