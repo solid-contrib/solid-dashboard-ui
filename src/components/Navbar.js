cb585e4f3f01f57005e71de6b83abec132f843d1
Daniel Friedman <dfriedman58@gmail.com>
2016-07-22 15:58:49 -0400

    Add some padding below the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,16 @@
 import React, { PropTypes } from 'react'
 
 const Navbar = ({ picURL }) => (
-  <nav className="navbar navbar-light bg-faded" role="navigation">
-    <a className="navbar-brand" href="/">Solid</a>
-    <img
-      className="img-circle pull-xs-right"
-      src={ picURL }
-      alt="Your profile picture"
-      width="30" />
-  </nav>
+  <div style={{ paddingBottom: '1rem' }}>
+    <nav className="navbar navbar-light bg-faded" role="navigation">
+      <a className="navbar-brand" href="/">Solid</a>
+      <img
+        className="img-circle pull-xs-right"
+        src={ picURL }
+        alt="Your profile picture"
+        width="30" />
+    </nav>
+  </div>
 )
 
 Navbar.propTypes = {
