9d1934766795a61982f402091cf4378d025f4a81
Daniel Friedman <dfriedman58@gmail.com>
2016-07-22 15:47:50 -0400

    Add link to warp

--- src/components/LoggedInDashboard.js
+++ src/components/LoggedInDashboard.js
@@ -4,27 +4,41 @@ import Card from './Card'
 
 const LoggedInDashboard = ({ profile }) => {
   return (
-    <div className="row">
-      <div className="col-sm-4">
-        <Card
-          title="Profile Builder"
-          url="https://solid.github.io/solid-profile-ui/"
-          description="Edit your solid profile."
-        />
+    <div>
+      <div className="row">
+        <div className="col-sm-4">
+          <Card
+            title="Profile Builder"
+            url="https://solid.github.io/solid-profile-ui/"
+            description="Edit your solid profile."
+          />
+        </div>
+        <div className="col-sm-4">
+          <Card
+            title="Connections"
+            url="https://solid.github.io/solid-connections-ui/"
+            description="Make Connections."
+          />
+        </div>
+        <div className="col-sm-4">
+          <Card
+            title="Inbox"
+            url="https://solid.github.io/solid-inbox/"
+            description="Read your messages."
+          />
+        </div>
       </div>
-      <div className="col-sm-4">
-        <Card
-          title="Connections"
-          url="https://solid.github.io/solid-connections-ui/"
-          description="Make Connections."
-        />
-      </div>
-      <div className="col-sm-4">
-        <Card
-          title="Inbox"
-          url="https://solid.github.io/solid-inbox/"
-          description="Read your messages."
-        />
+      <div className="row">
+        { profile.storage.length
+            ? <div className="col-sm-4">
+                <Card
+                  title="Storage"
+                  url={ profile.storage[0] }
+                  description="Access your files."
+                />
+              </div>
+            : ''
+        }
       </div>
     </div>
   )
