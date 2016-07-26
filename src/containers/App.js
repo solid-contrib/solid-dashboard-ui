db56a2253a7144e908f72cefc8cf3ed336219a09
Daniel Friedman <dfriedman58@gmail.com>
2016-07-26 16:12:45 -0400

    Add App spec

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,30 +1,23 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
 
-import * as Actions from '../actions'
 import Dashboard from './Dashboard'
 import Navbar from '../components/Navbar'
 
-let App = ({ profile, actions }) => (
+// export unconnected App for testing
+export const App = ({ profile }) => (
   <div>
-    <Navbar picURL={ profile.picture ||  "assets/img/solid-logo.svg" } />
+    <Navbar picURL={ profile.picture || 'assets/img/solid-logo.svg' } />
     <Dashboard />
   </div>
 )
 
-
 App.propTypes = {
-  profile: PropTypes.object.isRequired,
-  actions: PropTypes.object.isRequired
+  profile: PropTypes.object.isRequired
 }
 
 function mapStateToProps (state) {
   return { profile: state.profile }
 }
 
-function mapDispatchToProps (dispatch) {
-  return { actions: bindActionCreators(Actions, dispatch) }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(mapStateToProps)(App)
---
+++ test/containers/App.spec.js
@@ -0,0 +1,26 @@
+/* global describe, it */
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+import React from 'react'
+
+import Navbar from '../../src/components/Navbar'
+import { App } from '../../src/containers/App'
+import Dashboard from '../../src/containers/Dashboard'
+
+describe('App', () => {
+  it('renders a Navbar and a Dashboard', () => {
+    const wrapper = shallow(<App profile={{ picture: null }} />)
+    expect(wrapper.containsAllMatchingElements([
+      <Navbar picURL='assets/img/solid-logo.svg' />,
+      <Dashboard />
+    ])).to.equal(true)
+  })
+
+  it('passes the profile picture to the Navbar when defined', () => {
+    const picURL = 'http://example.com/pic.jpg'
+    const wrapper = shallow(<App profile={{ picture: picURL }} />)
+    expect(wrapper.containsMatchingElement(
+      <Navbar picURL={ picURL } />
+    )).to.equal(true)
+  })
+})
